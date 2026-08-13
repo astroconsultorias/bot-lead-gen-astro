68cd5ccda16a7141c3c59da9f70f9bfa1733a9fc
astroconsultorias <astroconsultorias@gmail.com>
2026-08-13 06:58:47 -0300

    Carrega estilos do novo banner fotográfico

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './astro-theme.css'
+import './astro-banner.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
